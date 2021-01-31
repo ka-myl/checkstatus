80450a7bfed9dcb17948eed1fe14c68d933a5756
kamyl <73426844+ka-myl@users.noreply.github.com>
2021-01-31 16:18:38 +0100

    Add getting sites list

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"checkstatus/sites"
+	"fmt"
+)
+
+var colorRed = "\033[31m"
+var colorGreen = "\033[32m"
+var colorReset = "\033[0m"
 
 func main() {
-	fmt.Println("Hello status checker!")
+	sites := sites.GetSitesList()
+
+	for _, site := range sites {
+		fmt.Println(site)
+	}
 }
+
+// func checkSiteStatus(url string) {
+// 	res, err := http.Get(url)
+
+// 	if err != nil {
+// 		fmt.Println(url, colorRed, "DOWN", colorReset)
+// 		return
+// 	}
+
+// 	fmt.Println(url, colorGreen, res.Status, colorReset)
+// }
---
+++ sites/sites.go
@@ -0,0 +1,41 @@
+package sites
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+var sitesFile = "E:\\DEV\\checkstatus\\sites.txt"
+
+func checkErr(e error, msg string) {
+	if e != nil {
+		fmt.Println(msg)
+		fmt.Println(e)
+		os.Exit(1)
+	}
+}
+
+// GetSitesList returns a list of sites that should be checked for status
+func GetSitesList() []string {
+	content, err := ioutil.ReadFile(sitesFile)
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			content = createDefaultSitesList()
+		} else {
+			checkErr(err, "Error while reading default sites list file")
+		}
+	}
+
+	return strings.Fields(string(content))
+}
+
+func createDefaultSitesList() []byte {
+	data := []byte("http://google.com\n")
+	err := ioutil.WriteFile(sitesFile, data, 0644)
+	checkErr(err, "Error while creating default sites list file")
+
+	return data
+}
